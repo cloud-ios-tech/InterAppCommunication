eca79e1bcff6635ac7c0bfc9aa4c9b7c15bf8838
Dmitry Golubev <dmitry.golubev@vk.team>
2023-11-03 17:24:24 +0300

    Update public headers

---
+++ InterAppCommunication/InterAppCommunication.h
@@ -0,0 +1,23 @@
+//
+//  InterAppCommunication.h
+//  InterAppCommunication
+//
+//  Created by dmitry.golubev on 03.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+#import <InterAppCommunication/IACClient.h>
+#import <InterAppCommunication/IACDelegate.h>
+#import <InterAppCommunication/IACManager.h>
+#import <InterAppCommunication/IACRequest.h>
+#import <InterAppCommunication/NSString+IACExtensions.h>
+
+//! Project version number for InterAppCommunication.
+FOUNDATION_EXPORT double InterAppCommunicationVersionNumber;
+
+//! Project version string for InterAppCommunication.
+FOUNDATION_EXPORT const unsigned char InterAppCommunicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InterAppCommunication/PublicHeader.h>
+
+
